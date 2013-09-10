22dd961ba325ebb3d8a5278e251ffe53955dd406
Ian Herbert <iant.herbert@gmail.com>
2013-09-10 18:50:15 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 setInterval(function() {
 
-	widget.system("./get_harvest_tasks.py ian@thirdmind.com ith383lt1 | ./get_task_notes.py | ./set_skype_mood.py", function(obj) {
+	widget.system("./get_harvest_tasks.py ****@**********.com ******** | ./get_task_notes.py | ./set_skype_mood.py", function(obj) {
 
 	});
 
